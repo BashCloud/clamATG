67fc1d4672d2e01c53cf1bdb6fc68ae33adc11ac
Pulkit Aggarwal <pulkit.cloud@gmail.com>
2017-06-22 16:09:00 +0530

    Startup Form Set to clamATG

--- ClamWinApp/Program.cs
+++ ClamWinApp/Program.cs
@@ -1,25 +1,3 @@
-// Name:        Program.cs
-// Product:     ClamWin Free Antivirus
-//
-// Author(s):      Garincho [garincho at users dot sourceforge dot net]
-//
-// Created:     2007/02/28
-// Copyright:   Copyright ClamWin Pty Ltd (c) 2005-2007
-// Licence:
-//   This program is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation; either version 2 of the License, or
-//   (at your option) any later version.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program; if not, write to the Free Software
-//   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
@@ -67,7 +45,8 @@ namespace ClamWinApp
 
             Application.EnableVisualStyles();            
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ClamWinMainForm(args));
+            //Application.Run(new ClamWinMainForm(args));
+            Application.Run(new clamATG());
 
             /*try
             {
